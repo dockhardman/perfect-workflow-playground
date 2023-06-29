3db8ed782070374b535e833d3b000dcd1bc90d37
Allen Chou <f1470891079@gmail.com>
2023-06-29 16:59:12 +0800

    Create markdown artifact.

--- prefect_workflow/shared/pc_info.py
+++ prefect_workflow/shared/pc_info.py
@@ -1,12 +1,14 @@
+from textwrap import dedent
 from typing import Dict
 import json
 
 from prefect import task
+from prefect.artifacts import create_markdown_artifact
 from rich import print
 import platform
 
 
-@task(name="Get PC Info", tags=["os"], version="0.1.1", persist_result=True)
+@task(name="Get PC Info", tags=["os"], version="0.2.0", persist_result=True)
 def get_pc_info(*args, **kwargs) -> Dict:
     pc_info = {
         "System": platform.system(),
@@ -16,6 +18,21 @@ def get_pc_info(*args, **kwargs) -> Dict:
         "Machine": platform.machine(),
         "Processor": platform.processor(),
     }
+    create_markdown_artifact(
+        markdown=dedent(
+            """
+            ## PC Information
+            | Key | Value |
+            | --- | --- |
+            | System | {System} |
+            | Node Name | {Node Name} |
+            | Release | {Release} |
+            | Version | {Version} |
+            | Machine | {Machine} |
+            | Processor | {Processor} |
+            """
+        ).format(**pc_info),
+    )
     pc_info = json.loads(json.dumps(pc_info))
     print("PC Information:", pc_info)
     return pc_info
