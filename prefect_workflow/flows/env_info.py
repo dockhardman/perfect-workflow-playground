0b68d109c8aa89bf366ffd7f66de5277dabdc7de
Allen Chou <f1470891079@gmail.com>
2023-06-29 12:29:32 +0800

    First deployment

---
+++ prefect_workflow/flows/env_info.py
@@ -0,0 +1,22 @@
+from typing import Dict, TYPE_CHECKING
+
+from prefect import flow
+from rich import print
+
+from prefect_workflow.shared.pc_info import get_pc_info
+
+if TYPE_CHECKING:
+    from prefect.futures import PrefectFuture
+
+
+@flow(name="Get Enviriment Information", version="0.1.0")
+def get_env_info(*args, **kwargs) -> Dict:
+    print(f"Args: {args}")
+    print(f"Kwargs: {kwargs}")
+
+    env_info = {}
+    pf_future: "PrefectFuture" = get_pc_info.submit(*args, **kwargs)
+
+    pc_info = pf_future.result(timeout=20.0)
+    env_info.update(**pc_info)
+    return env_info
---
+++ prefect_workflow/shared/pc_info.py
@@ -0,0 +1,21 @@
+from typing import Dict
+import json
+
+from prefect import task
+from rich import print
+import platform
+
+
+@task(name="Get PC Info", tags=["os"], version="0.1.0")
+def get_pc_info(*args, **kwargs) -> Dict:
+    pc_info = {
+        "System": platform.system(),
+        "Node Name": platform.node(),
+        "Release": platform.release(),
+        "Version": platform.version(),
+        "Machine": platform.machine(),
+        "Processor": platform.processor(),
+    }
+    pc_info = json.loads(json.dumps(pc_info))
+    print("PC Information:", pc_info)
+    return pc_info
